28f15ee49db50f459ce9653b99b2e34d5eff6e31
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-26 13:23:33 -0400

    linkedlist review added

---
+++ dsa-04-LinkedList/src/main/java/com/cydeo/LinkedList_Review/UserNode.java
@@ -0,0 +1,13 @@
+package com.cydeo.LinkedList_Review;
+
+public class UserNode {
+
+    String name;
+    String lastName;
+    UserNode next;
+
+    public UserNode(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
