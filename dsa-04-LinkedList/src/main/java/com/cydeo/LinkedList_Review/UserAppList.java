ae9c5cef559cb2a04e33ca304f6cc564700b5798
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-03-26 14:02:52 -0400

    linkedlist review added

--- dsa-04-LinkedList/src/main/java/com/cydeo/LinkedList_Review/UserAppList.java
+++ dsa-04-LinkedList/src/main/java/com/cydeo/LinkedList_Review/UserAppList.java
@@ -5,11 +5,16 @@ public class UserAppList {
     public static void main(String[] args) {
 
         UserLinkedList list = new UserLinkedList();
+
         list.insertLast(new UserNode("Carol", "Smith"));
         list.insertLast(new UserNode("Jack", "Reacher"));
         list.insertLast(new UserNode("John", "XXX"));
+        list.printNames(); // Carol=> Jack=> John=> null
+        list.deleteByName("Jack"); // Carol=> John=> null
         list.printNames();
-        list.deleteByName("Carol");
+        list.deleteByName("John"); // Carol=> null
+        list.printNames();
+        list.deleteByName("Carol"); // List is empty
         list.printNames();
     }
 }
