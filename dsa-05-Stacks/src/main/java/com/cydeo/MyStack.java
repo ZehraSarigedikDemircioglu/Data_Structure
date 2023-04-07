bff20553430acd1680f8ca3045a0c16568193b92
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-07 14:15:36 -0400

    stack commit

---
+++ dsa-05-Stacks/src/main/java/com/cydeo/MyStack.java
@@ -0,0 +1,27 @@
+package com.cydeo;
+
+public class MyStack<T> {
+
+    public SNode<T> bottom;
+    public SNode<T> top;
+    int size;
+
+    public boolean isEmpty(){
+        return bottom == null;
+    }
+
+    public void push(T item){
+
+        SNode<T> node = new SNode<>((SNode) item);
+
+        if(isEmpty()){
+            bottom = top = node;
+        }else{
+            top.next = node;
+            top = node;
+        }
+        size++;
+    }
+
+    //public T peek()
+}
