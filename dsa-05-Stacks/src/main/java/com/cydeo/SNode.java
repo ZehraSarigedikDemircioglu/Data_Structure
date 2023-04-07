f84ef7c18117c0fffa20e594b0eeb8df4b0c008f
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-07 16:46:31 -0400

    stack edited

--- dsa-05-Stacks/src/main/java/com/cydeo/SNode.java
+++ dsa-05-Stacks/src/main/java/com/cydeo/SNode.java
@@ -5,7 +5,7 @@ public class SNode<T> {
     public T value;
     SNode next;
 
-    public SNode(SNode next) {
-        this.next = next;
+    public SNode(T value) {
+        this.value = value;
     }
 }
