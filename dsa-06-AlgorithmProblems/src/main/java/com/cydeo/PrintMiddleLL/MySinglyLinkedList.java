81bdc9ce239b5e1051dcacb6615244671e53e05a
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-24 13:37:30 -0400

    methods added

--- dsa-06-AlgorithmProblems/src/main/java/com/cydeo/PrintMiddleLL/MySinglyLinkedList.java
+++ dsa-06-AlgorithmProblems/src/main/java/com/cydeo/PrintMiddleLL/MySinglyLinkedList.java
@@ -1,4 +1,25 @@
 package com.cydeo.PrintMiddleLL;
 
 public class MySinglyLinkedList {
+
+    Node head;
+    Node tail;
+    int size;
+
+    boolean isEmpty(){
+        return head == null;
+    }
+
+    void add(int data) {
+        Node node = new Node(data);
+
+        if (isEmpty()) { // if the list is empty
+            head = tail = node;
+            size++;
+        } else { // if there are elements in list
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+    }
 }
