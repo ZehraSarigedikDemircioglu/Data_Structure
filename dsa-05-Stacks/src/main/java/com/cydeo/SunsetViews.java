e269ffdbfd0a5f174182c3a27ae6bd83ac24fa58
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-24 12:21:21 -0400

    sunsetview solution added

--- dsa-05-Stacks/src/main/java/com/cydeo/SunsetViews.java
+++ dsa-05-Stacks/src/main/java/com/cydeo/SunsetViews.java
@@ -13,21 +13,32 @@ public class SunsetViews {
 
         System.out.println(sunsetViews(buildings, direction)); // [1, 3, 6, 7]
 
+        String direction2 = "WEST";
 
+        System.out.println(sunsetViews(buildings, direction2)); // [0, 1]
     }
 
     public static ArrayList<Integer> sunsetViews(int[] buildings, String direction) {
 
-        Stack<Integer> stack = new Stack<>();
+//        Stack<Integer> stack = new Stack<>();
+//
+//        if (buildings.length == 0) {
+//            return new ArrayList<>();
+//        }
+//
+//        for (int i = 0; i < buildings.length; i++) {
+//
+//            while (!stack.isEmpty() && buildings[i] >= buildings[stack.peek()]) {
+//                stack.pop();
+//            }
+//            stack.push(i);
+//        }
+//        return new ArrayList<Integer>(stack);
 
-        if (buildings.length == 0) {
-            return new ArrayList<>();
-        }
-
+        Stack<Integer> stack = new Stack<>();// stack to hold building indicies that can see sunset
         // this part is for East by default
         int i = 0;
         int step = 1;
-
         // this part is for west
         if (direction.equalsIgnoreCase("west")) {
             i = buildings.length - 1;
@@ -42,8 +53,9 @@ public class SunsetViews {
             i += step;
         }
         // now I have a stack with the building indices that can see sunset
-        if (direction.equalsIgnoreCase("west")) Collections.reverse(stack);
-
+        if (direction.equalsIgnoreCase("west")){
+            Collections.reverse(stack);
+        }
         return new ArrayList<Integer>(stack);
     }
 }
