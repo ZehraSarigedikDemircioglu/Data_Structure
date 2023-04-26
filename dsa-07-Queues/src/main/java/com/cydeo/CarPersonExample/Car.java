6d2eab5023608d5d46fec1bb37c1f7872dd5303f
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-26 10:36:59 -0400

    car person example added

---
+++ dsa-07-Queues/src/main/java/com/cydeo/CarPersonExample/Car.java
@@ -0,0 +1,12 @@
+package com.cydeo.CarPersonExample;
+
+public class Car {
+
+    String brand;
+    String type;
+
+    public Car(String brand, String type) {
+        this.brand = brand;
+        this.type = type;
+    }
+}
