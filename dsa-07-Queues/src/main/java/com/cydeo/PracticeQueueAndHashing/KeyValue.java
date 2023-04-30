d2453e5e2410ef8b284e5b400a96450f196516b5
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-04-30 12:53:19 -0400

    hash practice committed

---
+++ dsa-07-Queues/src/main/java/com/cydeo/PracticeQueueAndHashing/KeyValue.java
@@ -0,0 +1,12 @@
+package com.cydeo.PracticeQueueAndHashing;
+
+public class KeyValue<U, V> {
+
+    U key;
+    V value;
+
+    public KeyValue(U key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
