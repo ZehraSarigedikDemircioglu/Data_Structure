8e72b73c2b9ca9f497ed48461a86cc6476f9a64d
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-02 15:03:47 -0400

    added levelorder traversal method

--- dsa-08-Trees/src/main/java/com/cydeo/MyTree.java
+++ dsa-08-Trees/src/main/java/com/cydeo/MyTree.java
@@ -1,5 +1,8 @@
 package com.cydeo;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyTree {
     TNode root;
 
@@ -51,6 +54,34 @@ public class MyTree {
         inOrderTraversal(root.leftChild); // visit left subtree
         System.out.print(root.value + ", "); // visit root
         inOrderTraversal(root.rightChild); // visit right subtree
+    }
+    // Left - Root - Right
+    void postOrderTraversal(TNode root){
+        if(root == null) {
+            return;
+        }
+        postOrderTraversal(root.leftChild); // visit left subtree
+        postOrderTraversal(root.rightChild); // visit right subtree
+        System.out.print(root.value + ", "); // visit root
+    }
+
+    void levelOrderTraversal(TNode root){
+
+        if(root== null) {
+            return;
+        }
+        Queue<TNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            TNode toVisit = queue.poll();
+            System.out.print(toVisit.value + ", ");
+            if(toVisit.leftChild != null){
+                queue.add(toVisit.leftChild);
+            }
+            if(toVisit.rightChild != null){
+                queue.add(toVisit.rightChild);
+            }
+        }
     }
 
 
