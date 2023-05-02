5330dfe80bed3b3c836a566168b4e7ddddabf547
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-01 20:38:47 -0400

    added runner

--- dsa-08-Trees/src/main/java/com/cydeo/TreeApp.java
+++ dsa-08-Trees/src/main/java/com/cydeo/TreeApp.java
@@ -2,6 +2,13 @@ package com.cydeo;
 
 public class TreeApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MyTree tree = new MyTree();
+        int[] numbers = {10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
+
+        for (int i = 0; i < 11; i++) {
+            tree.insert(numbers[i]);
+        }
+        VisualizeTree.printTree(tree.root, null, false);
     }
 }
