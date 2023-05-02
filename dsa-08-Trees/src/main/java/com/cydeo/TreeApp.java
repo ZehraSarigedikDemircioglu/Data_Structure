56e4136357e9ded59989bf41142d00b5272c3cd7
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-02 15:03:58 -0400

    added runner

--- dsa-08-Trees/src/main/java/com/cydeo/TreeApp.java
+++ dsa-08-Trees/src/main/java/com/cydeo/TreeApp.java
@@ -12,5 +12,11 @@ public class TreeApp {
         VisualizeTree.printTree(tree.root, null, false);
 
         tree.preOrderTraversal(tree.root); // 10, 6, 4, 5, 8, 9, 20, 17, 42, 29, 47
+        System.out.println();
+        tree.inOrderTraversal(tree.root); // 4, 5, 6, 8, 9, 10, 17, 20, 29, 42, 47
+        System.out.println();
+        tree.postOrderTraversal(tree.root); // 5, 4, 9, 8, 6, 17, 29, 47, 42, 20, 10
+        System.out.println();
+        tree.levelOrderTraversal(tree.root); // 10, 6, 20, 4, 8, 17, 42, 5, 9, 29, 47
     }
 }
