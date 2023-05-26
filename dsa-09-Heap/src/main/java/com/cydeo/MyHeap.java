7d12ece6e2ff761c81e973eb0ab6e8e2653c640b
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-26 15:07:39 -0400

    updated

--- dsa-09-Heap/src/main/java/com/cydeo/MyHeap.java
+++ dsa-09-Heap/src/main/java/com/cydeo/MyHeap.java
@@ -12,9 +12,21 @@ public class MyHeap {
         this.size = 0;
     }
 
+    public MyHeap(int[] items) {
+        this.items = items;
+        this.size = items.length;
+    }
+
+    public int peek() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        return items[0];
+    }
+
     public void insert(int value) {
         if (size == items.length) {
-            throw new NoSuchElementException();
+            throw new IllegalStateException();
         } else {
             items[size++] = value;
             bubbleUp();
@@ -49,7 +61,7 @@ public class MyHeap {
     public void bubbleDown() {
         int index = 0;
         int largerChildIndex;
-        while (index <= size && isValidParent(index)) {
+        while (index <= size && !isValidParent(index)) {
             largerChildIndex = largerChildIndex(index);
             swap(index, largerChildIndex);
             index = largerChildIndex;
@@ -62,7 +74,7 @@ public class MyHeap {
         } else {
             boolean isValid = items[index] > items[leftChildIndex(index)];
             if (hasRightChild(index)) {
-                isValid = items[index] >= items[rightChildIndex(index)];
+                isValid= (items[index]>=items[leftChildIndex(index)]&&items[index]>=items[rightChildIndex(index)]);
             }
             return isValid;
         }
@@ -87,11 +99,11 @@ public class MyHeap {
     }
 
     public boolean hasLeftChild(int index) {
-        return leftChildIndex(index) < size;
+        return leftChildIndex(index) <= size;
     }
 
     public boolean hasRightChild(int index) {
-        return rightChildIndex(index) < size;
+        return rightChildIndex(index) <= size;
     }
 
     public int largerChildIndex(int index) {
