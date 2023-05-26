06815a7226c0599b3dec492d1946d565a75e10c2
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-26 12:45:23 -0400

    added remove method

--- dsa-09-Heap/src/main/java/com/cydeo/MyHeap.java
+++ dsa-09-Heap/src/main/java/com/cydeo/MyHeap.java
@@ -16,8 +16,33 @@ public class MyHeap {
         if (size == items.length) {
             throw new NoSuchElementException();
         } else {
-            items[size++]=value;
+            items[size++] = value;
             bubbleUp();
+        }
+    }
+
+    public void bubbleUp() {
+
+        // index = calculate start point
+        int index = size - 1;
+        // while index > 0 && parents is less thank child continue
+        // 1. swap the parent and child
+        // calculate next index = parent index
+        while (index > 0 && items[index] > items[parentIndex(index)]) {
+            swap(index, parentIndex(index));
+            // assign the next index with the parent index
+            index = parentIndex(index);
+        }
+    }
+
+    public int remove() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        } else {
+            int result = items[0];
+            items[0] = items[size - 1];
+            size--;
+            bubbleDown();
         }
     }
 
@@ -29,7 +54,7 @@ public class MyHeap {
         return index * 2 + 2;
     }
 
-    public int parentChildIndex(int index) {
+    public int parentIndex(int index) {
         return (index - 1) / 2;
     }
 
