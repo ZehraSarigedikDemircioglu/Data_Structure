99584e432ce2c22bff94203aad8341a14124d539
ZehraSarigedikDemircioglu <z.saridem@gmail.com>
2023-05-26 13:37:57 -0400

    added runner class

--- dsa-09-Heap/src/main/java/com/cydeo/HeapApp.java
+++ dsa-09-Heap/src/main/java/com/cydeo/HeapApp.java
@@ -9,13 +9,12 @@ public class HeapApp {
         heap.insert(52);
         heap.insert(45);
         heap.insert(81);
-        heap.printHeap();
-        heap.remove();
-        heap.printHeap();
+        heap.printHeap(); // 120, 81, 52, 45, 61
+        System.out.println(heap.remove());
+        System.out.println("Heap values after removing 120");
+        heap.printHeap(); // 81, 61, 52, 45
         heap.insert(130);
-        heap.printHeap();
-        heap.remove();
-        heap.printHeap();
+        heap.printHeap(); // 130, 81, 52, 45, 61
         int[] numbers = {1, 2, 5, 6, 8, 21, 0};
     }
 }
